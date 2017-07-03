c1c7d52c214fc4ebec5159a48320ffe801181817
Jose <solorzano202009@gmail.com>
2017-07-03 10:17:29 -0400

    Incluir gitignore

---
+++ application/views/inicio_alternative.php
@@ -0,0 +1,307 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="author" content="SemiColonWeb" />
+
+	<!-- Stylesheets
+	============================================= -->
+	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="<?php echo assets_url(); ?>css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo assets_url(); ?>css/style.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo assets_url(); ?>css/swiper.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo assets_url(); ?>css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo assets_url(); ?>css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo assets_url(); ?>css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo assets_url(); ?>css/magnific-popup.css" type="text/css" />
+
+	<link rel="stylesheet" href="<?php echo assets_url(); ?>css/responsive.css" type="text/css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<!-- Document Title
+	============================================= -->
+	<title>PROIT technologies</title>
+
+</head>
+
+<body class="stretched">
+
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix" style="background-color:#333333 !important;">
+
+		<!-- Header
+		============================================= -->
+		<header id="header" class="full-header dark">
+
+			<div id="header-wrap">
+
+				<div class="container clearfix">
+
+					<div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+
+					<!-- Logo
+					============================================= -->
+					<div id="logo">
+						<a href="inicio" class="standard-logo" data-dark-logo="<?php echo assets_url(); ?>images/logo-dark.png"><img src="<?php echo assets_url(); ?>images/logo.png" alt="Canvas Logo"></a>
+						<a href="inicio" class="retina-logo" data-dark-logo="<?php echo assets_url(); ?>images/logo-dark@2x.png"><img src="<?php echo assets_url(); ?>images/logo@2x.png" alt="Canvas Logo"></a>
+					</div><!-- #logo end -->
+
+					<!-- Primary Navigation
+					============================================= -->
+					<nav id="primary-menu" class="sub-title">
+
+						<ul>
+							<li class="current"><a href="inicio"><div><?php echo $this->lang->line('link_home'); ?></div><span><?php echo $this->lang->line('sub_text_link_home'); ?></span></a>
+								<ul>
+									<li><a href="nosotros"><div><?php echo $this->lang->line('link_nosotros'); ?></div></a></li>
+								</ul>
+							</li>
+							<li><a href="#"><div><?php echo $this->lang->line('link_mercados'); ?></div><span><?php echo $this->lang->line('sub_text_link_mercados'); ?></span></a>
+								<ul>
+									<li><a href="admin_p"><div><?php echo $this->lang->line('link_admin'); ?></div></a></li>
+									<li><a href="ambiente"><div><?php echo $this->lang->line('link_ambiente'); ?></div></a></li>
+									<li><a href="agricultura"><div><?php echo $this->lang->line('link_agricultura'); ?></div></a></li>
+									<li><a href="energia"><div><?php echo $this->lang->line('link_energia'); ?></div></a></li>
+									<li><a href="industria"><div><?php echo $this->lang->line('link_industria'); ?></div></a></li>
+									<li><a href="mar"><div><?php echo $this->lang->line('link_mar'); ?></div></a></li>
+									<li><a href="salud"><div><?php echo $this->lang->line('link_salud'); ?></div></a></li>
+									<li><a href="servicios_f"><div><?php echo $this->lang->line('link_servicios'); ?></div></a></li>
+									<li><a href="telecomunicaciones"><div><?php echo $this->lang->line('link_telecomunicaciones'); ?></div></a></li>
+									<li><a href="transporte"><div><?php echo $this->lang->line('link_transporte'); ?></div></a></li>
+									<li><a href="ciudades_i"><div><?php echo $this->lang->line('link_ciudades'); ?></div></a></li>
+								</ul>
+							</li>
+							<li><a href="#"><div><?php echo $this->lang->line('link_soluciones'); ?></div><span><?php echo $this->lang->line('sub_text_link_soluciones'); ?></span></a>
+								<ul>
+									<li><a href="soluciones"><div><?php echo $this->lang->line('link_soluciones_n'); ?></div></a></li>
+									<li><a href="intraestructura"><div><?php echo $this->lang->line('link_infraestructura'); ?></div></a></li>
+									<li><a href="seguridad"><div><?php echo $this->lang->line('link_seguridad'); ?></div></a></li>
+									<li><a href="plataformas"><div><?php echo $this->lang->line('link_plataformas'); ?></div></a></li>
+									<li><a href="redes_i"><div><?php echo $this->lang->line('link_redes'); ?></div></a></li>
+									<li><a href="gestion_s"><div><?php echo $this->lang->line('link_gestion'); ?></div></a></li>
+									<li><a href="outsourcing"><div><?php echo $this->lang->line('link_outsourcing'); ?></div></a></li>
+									<li><a href="productos"><div><?php echo $this->lang->line('link_productos'); ?></div></a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="contacto"><div><?php echo $this->lang->line('link_contactos'); ?></div><span><?php echo $this->lang->line('sub_text_link_contactos'); ?></span></a>
+							</li>
+							<li><a href="#"><div>Idiomas</div><span>Idiomas</span></a>
+								<ul>
+									<li>
+										<a onclick="javascript:window.location.href='<?php echo base_url(); ?>LanguageSwitcher/switchLang/portuguese';" href="#">
+										<?php if($this->session->userdata('site_lang') == 'portuguese') echo '<div style="color:#3DAAD6;">Pt</div>'; 
+										else echo '<div style="color:#FFFFFF;">Pt</div>';?>
+										</a>
+									</li>
+									<li>
+										<a onclick="javascript:window.location.href='<?php echo base_url(); ?>LanguageSwitcher/switchLang/spanish';" href="#">
+										<?php if($this->session->userdata('site_lang') == 'spanish') echo '<div style="color:#3DAAD6;">Es</div>'; 
+										else echo '<div style="color:#FFFFFF;">Es</div>';?>
+										</a>
+									</li>
+									<li>
+										<a onclick="javascript:window.location.href='<?php echo base_url(); ?>LanguageSwitcher/switchLang/english';" href="#">
+										<?php if($this->session->userdata('site_lang') == 'english') echo '<div style="color:#3DAAD6;">En</div>'; 
+										else echo '<div style="color:#FFFFFF;">En</div>';?>
+										</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+
+						<!-- Top Search
+						============================================= -->
+						<!--<div id="top-search">
+							<a href="#" id="top-search-trigger"><i class="icon-search3"></i><i class="icon-line-cross"></i></a>
+							<form action="search.html" method="get">
+								<input type="text" name="q" class="form-control" value="" placeholder="Type &amp; Hit Enter..">
+							</form>
+						</div>--><!-- #top-search end -->
+						
+						<!-- Top Search
+						============================================= -->
+						<!--<div id="top-cart">
+							<a onclick="javascript:window.location.href='<?php echo base_url(); ?>LanguageSwitcher/switchLang/portuguese';" href="#">
+							<?php if($this->session->userdata('site_lang') == 'portuguese') echo '<span style="color:#3DAAD6;">Pt</span>'; 
+							else echo '<span>Pt</span>';?>
+							</a>
+						</div>--><!-- #top-search end -->
+						
+						<!-- Top Search
+						============================================= -->
+						<!--<div id="top-cart">
+							<a onclick="javascript:window.location.href='<?php echo base_url(); ?>LanguageSwitcher/switchLang/spanish';" href="#">
+							<?php if($this->session->userdata('site_lang') == 'spanish') echo '<span style="color:#3DAAD6;">Es</span>'; 
+							else echo '<span>Es</span>';?>
+							</a>
+						</div>--><!-- #top-search end -->
+						
+						<!-- Top Search
+						============================================= -->
+						<!--<div id="top-cart">
+							<a onclick="javascript:window.location.href='<?php echo base_url(); ?>LanguageSwitcher/switchLang/english';" href="#">
+							<?php if($this->session->userdata('site_lang') == 'english') echo '<span style="color:#3DAAD6;">En</span>'; 
+							else echo '<span>En</span>';?>
+							</a>
+						</div>--><!-- #top-search end -->
+
+					</nav><!-- #primary-menu end -->
+
+				</div>
+
+			</div>
+
+		</header><!-- #header end -->
+
+		<section id="slider" class="slider-parallax swiper_wrapper full-screen force-full-screen clearfix">
+
+			<div class="slider-parallax-inner">
+
+				<div class="swiper-container swiper-parent">
+					<div class="swiper-wrapper">
+						
+						<div class="swiper-slide dark">
+							<div class="container clearfix">
+								<div class="slider-caption slider-caption-center">
+									<h2 data-caption-animate="fadeInUp">Sumamos Innovaciones a su Empresa.</h2>
+									<p data-caption-animate="fadeInUp" data-caption-delay="200">Tenemos una visión a 360º de las infraestructuras de IT, los recursos y la experiencia necesaria para transformar su negocio.</p>
+								</div>
+							</div>
+							<div class="video-wrap">
+								<video poster="<?php echo assets_url(); ?>images/videos/explore.jpg" preload="auto" loop autoplay muted>
+									<source src='<?php echo assets_url(); ?>images/videos/explore3.mp4' type='video/mp4' />
+								</video>
+								<div class="video-overlay" style="background-color: rgba(0,0,0,0.55);"></div>
+							</div>
+						</div>
+						<div class="swiper-slide dark">
+							<div class="container clearfix">
+								<div class="slider-caption slider-caption-center">
+									<h2 data-caption-animate="fadeInUp">¡Soluciones Innovadoras para Conquistar, Retener y Fidelizar Clientes!</h2>
+									<p data-caption-animate="fadeInUp" data-caption-delay="200">Nuestra mentalidad es apoyada por el conjunto de herramientas que nos da una nueva y poderosa manera de encontrar soluciones, desbloqueando el potencial de los recursos humanos de las empresas.</p>
+								</div>
+							</div>
+							<div class="video-wrap">
+								<video poster="<?php echo assets_url(); ?>images/videos/explore.jpg" preload="auto" loop autoplay muted>
+									<source src='<?php echo assets_url(); ?>images/videos/explore2.mp4' type='video/mp4' />
+								</video>
+								<div class="video-overlay" style="background-color: rgba(0,0,0,0.55);"></div>
+							</div>
+						</div>						
+						<div class="swiper-slide dark">
+							<div class="container clearfix">
+								<div class="slider-caption slider-caption-center">
+									<h2 data-caption-animate="fadeInUp">TIRE EL MÁXIMO PARTIDO DE SUS INFRAESTRUCTURAS</h2>
+									<p data-caption-animate="fadeInUp" data-caption-delay="200">¿Está preparado para la digitalización de su negocio? ¿En qué etapa está? ¿Están sus datos seguros y su infraestructura para responder de acuerdo con las necesidades de sus usuarios y clientes?</p>
+								</div>
+							</div>
+							<div class="video-wrap">
+								<video poster="<?php echo assets_url(); ?>images/videos/explore.jpg" preload="auto" loop autoplay muted>
+									<source src='<?php echo assets_url(); ?>images/videos/explore.mp4' type='video/mp4' />
+								</video>
+								<div class="video-overlay" style="background-color: rgba(0,0,0,0.55);"></div>
+							</div>
+						</div>
+
+
+					</div>
+					<div id="slider-arrow-left"><i class="icon-angle-left"></i></div>
+					<div id="slider-arrow-right"><i class="icon-angle-right"></i></div>
+					<div id="slide-number"><div id="slide-number-current"></div><span>/</span><div id="slide-number-total"></div></div>
+				</div>
+
+			</div>
+
+		</section>
+
+		<!-- Footer
+		============================================= -->
+		<footer id="footer" class="dark">
+
+			<!-- Copyrights
+			============================================= -->
+			<div id="copyrights">
+
+				<div class="container clearfix">
+
+					<div class="col_half">
+						<img src="<?php echo assets_url(); ?>images/footer-logo.png" alt="" class="footer-logo">
+						<?php echo $this->lang->line('rights_message'); ?>
+					</div>
+
+					<div class="col_half col_last tright">
+						<div class="copyrights-menu copyright-links fright clearfix">
+							<a href="inicio"><?php echo $this->lang->line('link_home_footer'); ?></a>/<a href="admin_p"><?php echo $this->lang->line('link_mercado_footer'); ?></a>/<a href="soluciones"><?php echo $this->lang->line('link_soluciones_footer'); ?></a>/<a href="contacto"><?php echo $this->lang->line('link_contact_footer'); ?></a>
+						</div>
+						<br>
+						<div class="fright clearfix col_last">
+							<a href="#" class="social-icon si-small si-borderless nobottommargin si-facebook">
+								<i class="icon-facebook"></i>
+								<i class="icon-facebook"></i>
+							</a>
+
+							<!--<a href="#" class="social-icon si-small si-borderless nobottommargin si-twitter">
+								<i class="icon-twitter"></i>
+								<i class="icon-twitter"></i>
+							</a>
+
+							<a href="#" class="social-icon si-small si-borderless nobottommargin si-gplus">
+								<i class="icon-gplus"></i>
+								<i class="icon-gplus"></i>
+							</a>
+
+							<a href="#" class="social-icon si-small si-borderless nobottommargin si-pinterest">
+								<i class="icon-pinterest"></i>
+								<i class="icon-pinterest"></i>
+							</a>
+
+							<a href="#" class="social-icon si-small si-borderless nobottommargin si-vimeo">
+								<i class="icon-vimeo"></i>
+								<i class="icon-vimeo"></i>
+							</a>
+
+							<a href="#" class="social-icon si-small si-borderless nobottommargin si-github">
+								<i class="icon-github"></i>
+								<i class="icon-github"></i>
+							</a>
+
+							<a href="#" class="social-icon si-small si-borderless nobottommargin si-yahoo">
+								<i class="icon-yahoo"></i>
+								<i class="icon-yahoo"></i>
+							</a>-->
+
+							<a href="#" class="social-icon si-small si-borderless nobottommargin si-linkedin">
+								<i class="icon-linkedin"></i>
+								<i class="icon-linkedin"></i>
+							</a>
+						</div>
+					</div>
+
+				</div>
+
+			</div><!-- #copyrights end -->
+
+		</footer><!-- #footer end -->
+
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="<?php echo assets_url(); ?>js/jquery.js"></script>
+	<script type="text/javascript" src="<?php echo assets_url(); ?>js/plugins.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="<?php echo assets_url(); ?>js/functions.js"></script>
+
+</body>
+</html>
