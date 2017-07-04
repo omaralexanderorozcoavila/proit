ecf8b1818d3b9fe14b3d0dc20ec50889c8439629
root <root@orozcodebian>
2017-07-04 16:23:52 -0400

    actividad 59

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.1.132/proit/';
+$config['base_url'] = 'http://10.9.35.117/proit/';
 
 /*
 |--------------------------------------------------------------------------
